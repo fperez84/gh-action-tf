e40514b62329581be4557a6d3de2e13960d3214d
Francisco Perez <francisco_perez@epam.com>
2026-03-04 10:10:00 -0300

    feat: add EC2 instance for Infracost cost demonstration

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -32,3 +32,18 @@ output "vpc_id" {
   description = "ID of the imported VPC"
   value       = data.aws_vpc.existing.id
 }
+
+output "ec2_instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.example.id
+}
+
+output "ec2_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.example.public_ip
+}
+
+output "ec2_private_ip" {
+  description = "Private IP address of the EC2 instance"
+  value       = aws_instance.example.private_ip
+}
